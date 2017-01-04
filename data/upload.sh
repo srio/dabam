3b44bb0b58734ead34161e3cf73804ecf0659dcd
Manuel Sanchez del Rio <srio@esrf.eu>
2017-01-04 18:30:11 +0100

    wulff files 43 and 44

--- data/upload.sh
+++ data/upload.sh
@@ -1,19 +1,5 @@
-scp dabam-037.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-037.dat
-scp dabam-037.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-037.txt
+scp dabam-043.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-043.dat
+scp dabam-043.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-043.txt
 
-scp dabam-038.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-038.dat
-scp dabam-038.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-038.txt
-
-scp dabam-039.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-039.dat
-scp dabam-039.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-039.txt
-
-scp dabam-040.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-040.dat
-scp dabam-040.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-040.txt
-
-scp dabam-041.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-041.dat
-scp dabam-041.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-041.txt
-
-scp dabam-042.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-042.dat
-scp dabam-042.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-042.txt
-
-
+scp dabam-044.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-044.dat
+scp dabam-044.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-044.txt
