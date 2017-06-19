c7fc638c946103982486a9f36271f1d54054f36c
Manuel Sanchez del Rio <srio@esrf.eu>
2017-06-19 10:42:51 +0200

    ROBL mirrors added

---
+++ work/CONVERT/robl/go.sh
@@ -0,0 +1,22 @@
+cp BM20-UB-M1-Up1-P5.slp    dabam-045.dat 
+cp ../../../data/dabam-001.txt dabam-045.txt 
+
+cp BM20-UB-M1_Pt-UP1-P5.slp dabam-046.dat 
+cp  dabam-045.txt  dabam-046.txt 
+
+cp BM20-UB-M1_Rh-UP1-P5.slp dabam-047.dat 
+cp  dabam-045.txt  dabam-047.txt 
+
+cp BM20-M2-Si-P8.slp dabam-048.dat 
+cp BM20-M2-Pt-P8.slp dabam-049.dat 
+cp BM20-M2-Rh-P8.slp dabam-050.dat 
+cp  dabam-045.txt  dabam-048.txt 
+cp  dabam-048.txt  dabam-049.txt 
+cp  dabam-048.txt  dabam-050.txt 
+
+cp g_M1-200mm-ABBA-ML1-1.slp dabam-051.dat 
+cp g_M1-200mm-ABBA-ML2-1.slp dabam-052.dat 
+cp g_M2-150mm-ABBA-ML1-1.slp dabam-053.dat 
+cp g_M2-150mm-ABBA-ML2-1.slp dabam-054.dat 
+cp  dabam-048.txt  dabam-051.txt 
+
