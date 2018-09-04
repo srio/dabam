e2cc375ae083eb7fc46b9d99b80c2665b44da9ae
Manuel Sanchez del Rio <srio@esrf.eu>
2018-09-04 14:55:07 +0200

    added id20 kb's

--- data/upload.sh
+++ data/upload.sh
@@ -1,5 +1,2 @@
-scp dabam-043.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-043.dat
-scp dabam-043.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-043.txt
-
-scp dabam-044.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-044.dat
-scp dabam-044.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/dabam-044.txt
+scp dabam-9??.dat srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/
+scp dabam-9??.txt srio@ftp:/ftp/anonymous/pub/scisoft/dabam/data/
---
+++ work/CONVERT/id20/README.txt
@@ -0,0 +1,18 @@
+FM3A (hutch1)
+=============
+B29-C01+Rh-cfg1-c2.slp 64
+B29-C01+Rh-cfg2-b3.slp 65
+
+FM4A 3 profiles:
+================
+FM4A-q0572-a7.slp       66
+FM4A-q0572-C-10-a7.slp  67
+FM4A-q0572-C+10-a7.slp  68
+
+
+FM3B (hutch2)
+=============
+b30-CO2Rh-cfg1-a3.slp  69
+B30-CO2Rh-cfg2-a2.slp  70
+
+FM4B not measured
