d231bc979809a546fdfb89f2e11a101fb4b2bee2
Manuel Sanchez del Rio <srio@esrf.eu>
2019-06-12 23:35:26 +0200

    added id15ML and ID27KB

---
+++ work/CONVERT/id27/go.sh
@@ -0,0 +1,13 @@
+#
+# id15 (ML substrate) and ID27 (old KB)
+#
+
+cp id15_ML_M1.txt ../../../data/dabam-079.txt
+cp id15_ML_M2.txt ../../../data/dabam-080.txt
+cp id27_hfm.txt ../../../data/dabam-081.txt
+cp id27_vfm.txt ../../../data/dabam-082.txt
+
+cp id15_ML_M1.dat ../../../data/dabam-079.dat
+cp id15_ML_M2.dat ../../../data/dabam-080.dat
+cp id27_hfm.dat ../../../data/dabam-081.dat
+cp id27_vfm.dat ../../../data/dabam-082.dat
